1ab30230aa8eb4246dba9d22b5596ee15b6725fd
Luffystar <poussin.axel22@gmail.com>
2018-06-26 11:15:35 +0200

    Connexion Fenetre Modifier

---
+++ src/fr/eni/clinique/IHM/login/AppliTestIHM.java
@@ -0,0 +1,14 @@
+package fr.eni.clinique.IHM.login;
+
+public class AppliTestIHM {
+	
+	public static void main (String[] args) throws Exception{
+
+		try {
+			Connexion main = new Connexion();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
