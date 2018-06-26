8c3e0d6dd00d7ed6518743c6d142ba73f0ec7558
Unknown <corentin.chartier.fr@gmail.com>
2018-06-26 14:40:36 +0200

    Frame + Langue
    - Ajour frame principal (WIP)
    - Ajout fichier de ressource texte

---
+++ src/fr/eni/clinique/IHM/controller/MainFrameController.java
@@ -0,0 +1,19 @@
+package fr.eni.clinique.IHM.controller;
+
+
+/*
+ * @author CHARTIER Corentin
+ */
+public class MainFrameController {
+    
+    //Attributs
+
+    //Constructeurs
+
+    // Methode
+
+    // Get
+
+    // Set
+
+}
